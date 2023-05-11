4c25f9c68480d8c5df26289f92f701341ab45f96
noornee <noornee.011@gmail.com>
2023-05-12 00:16:31 +0100

    chore: `nvim-tree` migrated to `on_attach`

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -7,17 +7,27 @@ local M = {
 }
 
 M.config = function()
+	local function my_on_attach(bufnr)
+		local api = require("nvim-tree.api")
+
+		local function opts(desc)
+			return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+		end
+
+		-- use all default mappings
+		api.config.mappings.default_on_attach(bufnr)
+
+		-- removes the default mapping; already have <C-t> mapped to toggle nvim-tree
+		vim.keymap.del("n", "<C-t>", { buffer = bufnr })
+
+		-- add your mappings
+		vim.keymap.set("n", "?", api.tree.toggle_help, opts("Help"))
+	end
+
 	local nvim_tree = require("nvim-tree")
 
 	nvim_tree.setup({
-		view = {
-			-- auto
-			mappings = {
-				list = {
-					{ key = "<C-t>", action = "" }, -- removes the default mapping; already have <C-t> mapped to toggle nvim-tree
-				},
-			},
-		},
+		on_attach = my_on_attach,
 	})
 end
 
