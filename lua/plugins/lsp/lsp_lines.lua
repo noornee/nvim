ad1767d3b118f6a5bdf23c4feac5fe676d1d37db
noornee <noornee.011@gmail.com>
2023-06-30 09:47:09 +0100

    fix: lsp_lines getting toggled in other filetypes.

--- lua/plugins/lsp/lsp_lines.lua
+++ lua/plugins/lsp/lsp_lines.lua
@@ -1,20 +1,13 @@
 local M = {
 	"https://git.sr.ht/~whynothugo/lsp_lines.nvim",
 	ft = "rust",
-	keys = {
-		{
-			"<leader>l",
-			"<cmd>lua require('lsp_lines').toggle()<CR>",
-			"Toggle lsp_lines",
-		},
-	},
 }
 
 M.config = function()
 	require("lsp_lines").setup({
+		vim.keymap.set("n", "<leader>l", ":lua require('lsp_lines').toggle()<CR>", { noremap = true, silent = true }), -- Toggle lsp lines
 		vim.diagnostic.config({
 			virtual_text = false, -- Disable virtual_text since it's redundant due to lsp_lines.
-			-- virtual_lines = { only_current_line = true },
 		}),
 	})
 end
