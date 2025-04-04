8980ea504f4addb34c31345e0918273be62891a5
noornee <noornee.011@gmail.com>
2025-04-04 19:46:58 +0100

    feat: add custom lsp keymaps.
    might remove them later when im comfortable with the default global
    ones. also, copied from `elianiva` github. love the way the keys were
    declared.

--- lua/core/utils.lua
+++ lua/core/utils.lua
@@ -1,20 +1,59 @@
 local M = {}
+M.lsp = {}
 
---scans the specified directory while ignoring any file matching the ignore pattern.
---returns a table containing the contents of the directory
----@param directory string
----@param ignore? string this is optional
----@return table
-M.scandir = function(directory, ignore)
-	local i, t = 0, {}
-	ignore = ignore or ""
-	local pfile = io.popen("ls " .. directory .. " --ignore=" .. ignore)
-	for filename in pfile:lines() do
-		i = i + 1
-		t[i] = filename
-	end
-	pfile:close()
-	return t
+M.lsp.custom_mappings = function(bufnr)
+	vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {
+		desc = "Go to symbol definition",
+		noremap = true,
+		silent = true,
+		buffer = bufnr,
+	})
+
+	vim.keymap.set("n", "K", vim.lsp.buf.hover, {
+		desc = "Trigger hover window from the language server",
+		noremap = true,
+		silent = true,
+		buffer = bufnr,
+	})
+
+	vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation, {
+		desc = "Go to Implementation",
+		noremap = true,
+		silent = true,
+		buffer = bufnr,
+	})
+
+	vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {
+		desc = "Show references",
+		noremap = true,
+		silent = true,
+		buffer = bufnr,
+	})
+
+	vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {
+		desc = "Rename current symbol",
+		noremap = true,
+		silent = true,
+		buffer = bufnr,
+	})
+
+	vim.keymap.set("n", "<leader>f", function()
+		vim.lsp.buf.format({ async = true })
+	end, {
+		desc = "format file",
+		noremap = true,
+		silent = true,
+		buffer = bufnr,
+	})
+
+	vim.keymap.set("n", "<leader>ih", function()
+		vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+	end, {
+		desc = "Toggle Inlay Hints",
+		noremap = true,
+		silent = true,
+		buffer = bufnr,
+	})
 end
 
 return M
