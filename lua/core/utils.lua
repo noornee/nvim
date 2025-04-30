47fd480692b75d5869d674fc5b239a39902c7d66
noornee <noornee.011@gmail.com>
2025-04-30 12:04:48 +0100

    feat: add a custom util func to check if cursor is in struct

--- lua/core/utils.lua
+++ lua/core/utils.lua
@@ -1,6 +1,40 @@
 local M = {}
 M.lsp = {}
 
+--- Checks if the cursor is currently inside a struct context.
+-- This includes being within a struct's field declaration list
+-- or inside a struct instantiation (composite_literal).
+-- @return boolean true if cursor is in a struct-related context, else false.
+M.is_cursor_in_struct = function()
+	local ts_utils = require("nvim-treesitter.ts_utils")
+	local node = ts_utils.get_node_at_cursor()
+
+	-- Walk up the syntax tree to find the relevant node
+	while node do
+		local type = node:type()
+
+		-- Check if we're inside a composite_literal (struct instantiation)
+		if type == "composite_literal" then
+			-- Check if the parent node is an expression list, which indicates we're inside a struct initialization
+			local parent = node:parent()
+			if parent and parent:type() == "expression_list" then
+				return true -- We are inside a struct instantiation block
+			end
+		end
+
+		-- If we're inside the field_declaration_list node, it's part of a struct's fields
+		if type == "field_declaration_list" then
+			return true -- We're inside the field declaration list of a struct
+		end
+
+		-- Move up to the parent node to continue searching
+		node = node:parent()
+	end
+
+	-- If we're not inside a struct or its field list, return false
+	return false
+end
+
 M.lsp.custom_mappings = function(bufnr)
 	vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {
 		desc = "Go to symbol definition",
